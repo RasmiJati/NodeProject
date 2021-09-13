37df678de70193374aafe03f2f8bb9659268c972
Rasmi Jati <jatirasmi65@gmail.com>
2021-09-13 14:12:35 +0545

    change in server.js

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.listen(3000,  function(){
     console.log(`listening at 3000`)
 });
+
+/*
+//how route work
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
+app.post('/', (req, res) => {
+    res.send('Hello World Post from rj!')   //using insomnia
+});
 app.get('/hello', (req, res) => {
     res.send('Hello World from rj!')
+});
+ */
+
+//how to send parameter
+//query parameter(multiple object can be passed)
+ app.get("/", function(req,res){
+     var obj = req.query;
+     //res.send( obj)
+        res.send("HelloQuery" + obj.name)
+ });
+ //path parameter
+app.get("/project/:id", function(req,res){
+    var obj = req.params;
+   // res.send("Hello" + obj.id)
+    //select *from project where id = obj.id
+    res.send("ID is " + obj.id)
+});
+//parameter that comes in body when post and put request is done
+app.post("/project/:id", function(req,res){
+    var obj = req.body;
+    res.send("Hello Post" + obj.name)
 });
